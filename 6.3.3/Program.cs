07dc360676ceed500b0a87a6856f9a41187a8f0d
Anton-Molokov <m2306393@edu.misis.ru>
2024-03-29 23:08:53 +0300

    6 done

---
+++ 6.3.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    struct Team
+    {
+        public string Name { get; set; }
+        public int[] Scores { get; set; }
+
+        public Team(string name, int[] scores)
+        {
+            Name = name;
+            Scores = scores;
+        }
+
+        public int TotalScore => Scores.Select((score, index) => score * (6 - index)).Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        List<Team> teams = new List<Team>
+        {
+            new Team("Команда A", new int[] { 1, 2, 3, 4, 5, 6 }),
+            new Team("Команда B", new int[] { 2, 3, 4, 5, 6, 1 }),
+            new Team("Команда C", new int[] { 3, 4, 5, 6, 1, 2 })
+        };
+
+        Team winningTeam = teams.OrderByDescending(t => t.TotalScore).First();
+
+        Console.WriteLine($"Команда-победитель: {winningTeam.Name} с общим количеством баллов: {winningTeam.TotalScore}");
+    }
+}

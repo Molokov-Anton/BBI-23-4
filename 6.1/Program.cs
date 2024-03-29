f71a0e786ec0c31c1fcddbf07cbee6269bf12578
Anton-Molokov <m2306393@edu.misis.ru>
2024-03-29 13:12:04 +0300

    6 done

---
+++ 6.1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    struct Student
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int MissedLessons { get; set; }
+
+        public Student(string name, int score, int missedLessons)
+        {
+            Name = name;
+            Score = score;
+            MissedLessons = missedLessons;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student("Алексей", 2, 5),
+            new Student("Иван", 3, 3),
+            new Student("Мария", 2, 5),
+            new Student("Сергей", 4, 2),
+            new Student("Анна", 5, 1),
+            new Student("Дмитрий", 2, 4)
+        };
+
+        // Фильтрация студентов с оценкой 2
+        List<Student> failedStudents = new List<Student>();
+        foreach (var student in students)
+        {
+            if (student.Score == 2)
+            {
+                failedStudents.Add(student);
+            }
+        }
+
+        // Сортировка по количеству пропущенных занятий в порядке убывания
+        failedStudents.Sort((x, y) => y.MissedLessons.CompareTo(x.MissedLessons));
+
+        // Вывод результатов
+        foreach (var student in failedStudents)
+        {
+            Console.WriteLine($"{student.Name} - пропустил {student.MissedLessons} занятий");
+        }
+    }
+}
---
+++ 6.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    struct Competitor
+    {
+        public string Surname { get; set; }
+        public int[] Scores { get; set; }
+
+        public Competitor(string surname, int[] scores)
+        {
+            Surname = surname;
+            Scores = scores;
+        }
+
+        public int TotalScore => Scores.Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        List<Competitor> competitors = new List<Competitor>
+        {
+            new Competitor("Иванов", new int[] { 8, 9 }),
+            new Competitor("Петров", new int[] { 7, 8 }),
+            new Competitor("Сидоров", new int[] { 9, 8 }),
+            new Competitor("Кузнецов", new int[] { 8, 7 }),
+            new Competitor("Смирнов", new int[] { 7, 9 })
+        };
+
+        competitors.Sort((x, y) => y.TotalScore.CompareTo(x.TotalScore));
+
+        // Вывод результатов
+        for (int i = 0; i < competitors.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {competitors[i].Surname} - {competitors[i].TotalScore} баллов");
+        }
+    }
+}

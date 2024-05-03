397a51a5fbe48859ff522dc8733e9509e261de23
Anton-Molokov <m2306393@edu.misis.ru>
2024-05-03 19:47:21 +0300

    6.3 fixed

--- 6.3.3/Program.cs
+++ 6.3.3/Program.cs
@@ -28,6 +28,15 @@ class Program
         {
             Console.WriteLine($"Команда: {Name}, Место: {Rank}, Общее количество баллов: {TotalScore}");
         }
+
+        public void PrintParticipants()
+        {
+            Console.WriteLine($"Участники команды {Name}:");
+            for (int i = 0; i < Scores.Length; i++)
+            {
+                Console.WriteLine($"Место: {Scores[i]}, Участник: {i + 1}");
+            }
+        }
     }
 
     static void Main(string[] args)
@@ -39,21 +48,18 @@ class Program
             new Team("Команда C", new int[] { 3, 4, 5, 6, 1, 2 })
         };
 
-
         var sortedTeams = teams.OrderByDescending(t => t.TotalScore).ToList();
-
 
         List<Team> rankedTeams = new List<Team>();
         for (int i = 0; i < sortedTeams.Count; i++)
         {
-
             rankedTeams.Add(new Team(sortedTeams[i].Name, sortedTeams[i].Scores) { Rank = i + 1 });
         }
-
 
         foreach (var team in rankedTeams)
         {
             team.PrintDetails();
+            team.PrintParticipants();
         }
     }
 }
